c1994e8c434f59cf640704da62afa9ae9c86f89a
snthd <158rune@gmail.com>
2013-05-16 13:28:05 +0900

    Initial Commit

---
+++ ShortCom/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/16.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
