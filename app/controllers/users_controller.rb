788fc00453baabfafa582014037847a0d21a5111
snthd <158rune@gmail.com>
2013-05-24 16:08:34 +0900

    hehe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,8 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    @user.image = params[@tempfile]
+    @user[:image] = aoeuaosnh
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130523070132) do
+ActiveRecord::Schema.define(version: 20130524060538) do
 
   create_table "boards", force: true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20130523070132) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.binary   "image"
   end
 
 end
